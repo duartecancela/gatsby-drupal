9ca8cae433e6d5636537882d7bca73f52508ec2e
duartecancela <duartecancela@gmail.com>
2020-07-15 21:57:41 +0100

    Add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react"
-import { Link, graphql, StaticQuery } from "gatsby"
-
+import {Link, graphql, StaticQuery} from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/layout"
 
 
 const IndexPage = () => (
-  <StaticQuery
-    query={graphql`
+    <StaticQuery
+        query={graphql`
       query {
         allNodeArticle {
           edges {
@@ -19,11 +19,12 @@ const IndexPage = () => (
               relationships {
                 field_image {
                   localFile {
-                    childImageSharp {
-                      fluid(maxWidth: 400, quality: 100) {
-                        ...GatsbyImageSharpFluid
-                      }
-                    }
+                    url
+                     childImageSharp {
+                                  fluid(maxWidth: 400, quality: 100) {
+                                    ...GatsbyImageSharpFluid
+                                  }
+                     }
                   }
                 }
               }
@@ -32,22 +33,24 @@ const IndexPage = () => (
         },
       }
     `}
-    render={data => (
-      <Layout>
-        {data.allNodeArticle.edges.map(edge => (
-  <>
-    <h3><Link to={ edge.node.id }>{ edge.node.title }</Link></h3>
-    <small><em>{ Date(edge.node.created) }</em></small>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, width: `100%` }}>
-      {/* <Img fluid={ edge.node.relationships.field_image.localFile.childImageSharp.fluid } /> */}
-    </div>
-    <div dangerouslySetInnerHTML={{ __html: edge.node.body.value.split(' ').splice(0, 50).join(' ') + '...' }}></div>
-  </>
-))}
-      </Layout>
-    )}
-  />
+        render={data => (
+            <Layout>
+                {data.allNodeArticle.edges.map(edge => (
+                    <>
+                        <h3><Link to={edge.node.id}>{edge.node.title}</Link></h3>
+                        <small><em>{Date(edge.node.created)}</em></small>
+                        <div style={{maxWidth: `300px`, marginBottom: `1.45rem`, width: `100%`}}>
+                            <Img fluid={edge.node.relationships.field_image.localFile.childImageSharp.fluid}/>
+                        </div>
+                        <div
+                            dangerouslySetInnerHTML={{__html: edge.node.body.value.split(' ').splice(0, 50).join(' ') + '...'}}/>
+                    </>
+                ))}
+            </Layout>
+        )}
+    />
 )
 
 export default IndexPage
+
 
